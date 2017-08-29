62d2bc5ce5a4790434d1cc8c839d5d2c022d4fd2
Sean Law <sean.law.321@gmail.com>
2017-08-29 22:39:07 +1000

    Run gulp to lint & build js, run jekyll build & serve site
    Tutorial - https://aaronlasseigne.com/2016/02/03/using-gulp-with-jekyll/

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var eslint = require('gulp-eslint');
-var shell = require('gulp-shell');
-var browserSync = require('browser-sync');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+const child = require('child_process');
+const gutil = require('gulp-util');
+const browserSync = require('browser-sync').create();
+
+/** Default task:
+ *    - lint & compile js
+ *    - build jekyll site
+ *    - serve site using BrowserSync
+ */
+gulp.task('default', ['js-watch', 'jekyll', 'serve']);
 
 /* JS Linting */
-gulp.task('js-lint', function() {
+gulp.task('js-lint', () => {
   return gulp.src('src/js/index.js')
     .pipe(eslint())
     .pipe(eslint.format())
@@ -13,28 +21,42 @@ gulp.task('js-lint', function() {
 });
 
 /* Compile ES6 to ES5 */
-gulp.task('js-build', function() {
+gulp.task('js-build', () => {
   return gulp.src('src/js/index.js')
     .pipe(babel({presets: ['env']}))
     .pipe(gulp.dest('dist/js'));
 });
 
-/* Build jekyll site. Jekyll builds site into '_site/' */
-gulp.task('jekyll-build', shell.task(['jekyll build --watch']));
-
-/** Serve site:
- * 1. Rebuild site using jekyll on any file change
- * 2. Re-serve site using browser sync on any change in '_site'
- */
-gulp.task('serve', function() {
-  browserSync.init({
-    server: {baseDir: '_site/'}
-  });
-  gulp.watch('_site/**/*.*').on('change', browserSync.reload);
-});
-
-gulp.task('js-watch', function() {
+gulp.task('js-watch', () => {
   return gulp.watch('src/js/index.js', ['js-lint', 'js-build']);
 });
 
-gulp.task('default', ['js-watch', 'jekyll-build', 'serve']);
+/* Build jekyll */
+gulp.task('jekyll', () => {
+  const jekyll = child.spawn('jekyll', [
+    'build',
+    '--watch'
+  ]);
+
+  const jekyllLogger = (buffer) => {
+    buffer.toString()
+      .split(/\n/)
+      .forEach((message) => gutil.log('Jekyll: ' + message));
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+});
+
+/* Livereload using BrowserSync */
+gulp.task('serve', () => {
+  const siteRoot = '_site/';
+  browserSync.init({
+    files: [siteRoot + '**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+});
+
